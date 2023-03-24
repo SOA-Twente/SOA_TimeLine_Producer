4d97cce1a930704defbe2efb6a9f0abf14b776d6
Yun <81588081+YunFeng-Zheng@users.noreply.github.com>
2023-03-24 16:54:41 +0100

    Add files via upload

---
+++ Timeline-Producer/src/main/java/com/example/TimelineProducer/TimelineProducerApplication.java
@@ -0,0 +1,13 @@
+package com.example.TimelineProducer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TimelineProducerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TimelineProducerApplication.class, args);
+	}
+
+}
---
+++ Timeline-Producer/src/test/java/com/example/TimelineProducer/TimelineProducerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.TimelineProducer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TimelineProducerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
